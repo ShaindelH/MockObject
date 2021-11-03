b566ac8c6d673454f84c88afcc642ec96d6a4a7c
Shaindy Chaimson <shaimson@student.touro.edu>
2021-11-03 19:24:08 -0400

    Invalid Probability Exception class added

---
+++ src/InvalidProbabilityException.java
@@ -0,0 +1,11 @@
+
+public class InvalidProbabilityException extends Exception{
+
+	public InvalidProbabilityException() {
+		super();
+	}
+	
+	public InvalidProbabilityException(String message) {
+		super(message);
+	}
+}
